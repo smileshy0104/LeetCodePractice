6e3db8fd53cb47104dfd1c312b90f1893d879caa
yuyansong <1259581033@qq.com>
2025-04-21 20:54:23 +0800

    202 快乐数

--- MethodClassification/5-Hash-Map/0202_happy-number.go
+++ MethodClassification/5-Hash-Map/0202_happy-number.go
@@ -5,7 +5,7 @@ import "fmt"
 // 主函数入口
 func main() {
 	// 调用isHappy函数判断数字2是否为快乐数
-	fmt.Println(isHappy(2))
+	fmt.Println(isHappy0(19))
 }
 
 // isHappy判断一个整数是否为快乐数
@@ -24,6 +24,38 @@ func isHappy(n int) bool {
 	}
 	// 如果n最终等于4，则非快乐数
 	return false
+}
+
+// TODO 优化：使用map记录出现过的数字，以检测循环。（若出现在map中那么意味着不存在快乐数）
+// isHappy0 判断一个整数是否为快乐数。
+// 快乐数定义：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，
+// 如果经过多次替换后，数字最终变为1，则该整数为快乐数；如果最终进入循环，则不是快乐数。
+// 参数:
+//
+//	n - 需要判断的整数。
+//
+// 返回值:
+//
+//	如果输入的整数是快乐数，则返回true；否则返回false。
+func isHappy0(n int) bool {
+	// 使用map记录出现过的数字，以检测循环。
+	valueMap := make(map[int]bool)
+
+	// 循环直到n变为1，或者检测到循环。
+	for n != 1 {
+		// 如果当前数字已经出现过，则说明进入循环，不是快乐数。
+		if valueMap[n] {
+			return false
+		}
+		// 将当前数字标记为已出现。
+		valueMap[n] = true
+
+		// 计算下一个数字，即当前数字每个位置上的数字的平方和。
+		n = getSum(n)
+	}
+
+	// 如果退出循环，说明数字最终变为1，是快乐数。
+	return true
 }
 
 // getSum计算一个整数的每位数字平方和
