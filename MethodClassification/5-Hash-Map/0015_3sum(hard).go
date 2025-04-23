d893774d758cff13cc9b2db48865890f100466a6
yuyansong <1259581033@qq.com>
2025-04-23 20:53:49 +0800

    15 三数之和

--- MethodClassification/5-Hash-Map/0015_3sum(hard).go
+++ MethodClassification/5-Hash-Map/0015_3sum(hard).go
@@ -5,12 +5,7 @@ import (
 	"sort"
 )
 
-func main() {
-	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4}))
-	fmt.Println(threeSum([]int{0, 1, 1}))
-	fmt.Println(threeSum([]int{0, 0, 0}))
-}
-
+// threeSum 函数
 func threeSum(nums []int) [][]int {
 	// 边界条件检查：如果数组长度小于3，直接返回空结果
 	if len(nums) < 3 {
@@ -35,6 +30,7 @@ func threeSum(nums []int) [][]int {
 		for left < right {
 			sum := nums[i] + nums[left] + nums[right]
 
+			// 根据sum的值，调整指针位置或记录结果
 			if sum == 0 {
 				// 找到符合条件的三元组
 				ret = append(ret, []int{nums[i], nums[left], nums[right]})
@@ -62,5 +58,13 @@ func threeSum(nums []int) [][]int {
 		}
 	}
 
+	// 返回所有找到的三元组
 	return ret
+}
+
+func main() {
+	// 示例调用，验证threeSum函数的正确性
+	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4}))
+	fmt.Println(threeSum([]int{0, 1, 1}))
+	fmt.Println(threeSum([]int{0, 0, 0}))
 }
